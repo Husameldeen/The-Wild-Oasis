81077093c018c1506d56aa386d614eca84d04c28
Husameldeen <mhusam87@gmail.com>
2026-05-11 22:42:35 +0200

    add sort methods to booking and start implement pagination on bookings page

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -3,6 +3,7 @@ import { useSearchParams } from "react-router-dom";
 import { getBookings } from "../../services/apiBookings";
 
 export function useBookings() {
+  // FILTER BOOLINGS BASED ON SEARCH PARAMS
   const [searchParams] = useSearchParams();
 
   const filterValue = searchParams.get("status");
@@ -10,15 +11,22 @@ export function useBookings() {
   const filter =
     !filterValue || filterValue === "all"
       ? null
-      : { field: "status", value: filterValue };
+      : { field: "status", value: filterValue, method: "eq" };
+
+  // SORT BOOLINGS BASED ON SEARCH PARAMS
+  const sortByRow = searchParams.get("sortBy") || "startDate-desc";
+
+  const [field, direction] = sortByRow.split("-");
+
+  const sortBy = { field, direction };
 
   const {
     data: bookings,
     isLoading,
     error,
   } = useQuery({
-    queryKey: ["bookings", filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { bookings, isLoading, error };
